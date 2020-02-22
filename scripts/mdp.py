16748fb57d81d77946e901fe896ed3dcfefcf786
Pawel Gajewski <plglubiluk@p2025.prometheus>
2020-02-22 22:22:44 +0100

    first mdp + base filter

--- scripts/mdp.py
+++ scripts/mdp.py
@@ -1,22 +1,79 @@
 #!/usr/bin/env python
 
 import rospy
-import sensor_msgs.msg
-from cembra.srv import Action
+from sensor_msgs.msg import Image
+from cembra.srv import Action, ActionResponse
+from tmc_msgs.msg import JointVelocity
+from geometry_msgs.msg import Twist
 
 CAMERA_TOPIC = '/hsrb/head_rgbd_sensor/rgb/image_rect_color'
+VELOCITY_TOPIC = '/cembra/velocity'
+TWIST_TOPIC = '/cembra/twist'
+FAST = 0.3
+SLOW = 0.1
+STOP = 0.0
+
+vel_pub = None
+twist_pub = None
 
 def get_image():
-    msg = rospy.wait_for_message(CAMERA_TOPIC, sensor_msgs.msg.Image)
+    msg = rospy.wait_for_message(CAMERA_TOPIC, Image)
     print(msg)
 
-def handle_action():
-    pass
+def handle_action(msg):
+    actions = msg.action
+    
+    handle_velocity_actions(actions[:-1])
+    handle_twist_action(actions[-1])
+
+    # Let things happen for a while
+    rospy.sleep(0.02)
+
+    return ActionResponse(0)
+
+def handle_velocity_actions(actions):
+    assert(len(actions) == 5)
+
+    msg = JointVelocity()
+    msg.name = [
+            'arm_flex_joint',
+            'arm_lift_joint',
+            'arm_roll_joint',
+            'wrist_flex_joint',
+            'wrist_roll_joint'
+            ]
+    msg.velocity = [action_to_velocity(a) for a in actions]
+    msg.header.stamp = rospy.Time.now()
+
+    vel_pub.publish(msg)
+
+def handle_twist_action(action):
+    msg = Twist()
+    msg.angular.z = action_to_velocity(action)
+    
+    twist_pub.publish(msg)
+
+def action_to_velocity(action):
+    return {
+        0: STOP,
+        1: SLOW,
+        2: FAST
+    }[action]
 
 def run():
     rospy.init_node('mdp')
+
+    # initialize ROS publisher
+    # queue size of one will keep only the newest message
+    vel_pub = rospy.Publisher(VELOCITY_TOPIC, JointVelocity, queue_size=1)
+    twist_pub = rospy.Publisher(TWIST_TOPIC, Twist, queue_size=1)
+
+    # wait to establish connection between the controller
+    while vel_pub.get_num_connections() == 0 or twist_pub.get_num_connections() == 0:
+        rospy.sleep(0.1)
+
     s = rospy.Service('action', Action, handle_action)
-    print "Ready to execute actions."
+    print("MDP ready")
     rospy.spin()
 
 
---
+++ src/base_filter.cpp
@@ -0,0 +1,93 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <cmath>
+#include <tf2_ros/transform_listener.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf/transform_datatypes.h>
+
+const double MAX_ROTATION = M_PI / 2;
+
+ros::Publisher pub;
+tf2_ros::Buffer tfBuffer;
+tf2_ros::TransformListener tfListener(tfBuffer);
+
+double reference_rotation;
+double current_rotation;
+
+const double base = M_PI;
+
+double getCurrentRotation()
+{
+    geometry_msgs::TransformStamped trans;
+
+    try{
+      trans = tfBuffer.lookupTransform("map", "base_link", ros::Time(0), ros::Duration(10));
+    }
+    catch (tf2::TransformException &ex) {
+      ROS_WARN("%s",ex.what());
+    }
+    
+    return tf::getYaw(trans.transform.rotation);
+}
+
+bool isTwistSafe(const geometry_msgs::Twist &msg)
+{
+    double velocity = msg.angular.z;
+    double current_diff = base - fabs(fmod(fabs(reference_rotation - current_rotation), (2.0 * base)) - base);
+
+    // Sample future positions
+    for (int j = 1; j <= 10; ++j)
+    {
+        double predicted_diff = current_rotation + velocity * 0.1 * j;
+
+        bool is_safe = (predicted_diff >= MAX_ROTATION && predicted_diff >= current_diff);
+
+        if (!is_safe)
+        {
+            ROS_INFO_STREAM("Twist not safe");
+            return false;
+        }
+    }
+
+    return true;
+}
+
+void twistCallback(const geometry_msgs::Twist &msg)
+{
+    current_rotation = getCurrentRotation();
+    
+    if (isTwistSafe(msg))
+    {
+        pub.publish(msg);
+        // ROS_INFO("Safe twist");
+        return;
+    }
+
+    // ROS_INFO("Unsafe velocity");
+
+    return;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "base_filter");
+
+    // Setup reference rotation
+    reference_rotation = getCurrentRotation();
+
+    // Setup
+    ros::NodeHandle n;
+
+    pub = n.advertise<geometry_msgs::Twist>("/hsrb/command_velocity", 1);
+
+    while (pub.getNumSubscribers() == 0)
+    {
+        ros::Duration(0.1).sleep();
+    }
+
+    ros::Subscriber sub = n.subscribe("/cembra/twist", 1, twistCallback);
+    ROS_INFO("Ready to filter twist.");
+    ros::waitForShutdown();
+
+    return 0;
+}
