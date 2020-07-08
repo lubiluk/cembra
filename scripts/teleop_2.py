5cdf031841d105d892fd7f798d8ac6598bfa20b7
Pawel Gajewski <plglubiluk@p1739.prometheus>
2020-07-08 20:49:13 +0200

    key mapping

--- scripts/teleop_2.py
+++ scripts/teleop_2.py
@@ -5,6 +5,25 @@ import cembra.srv
 import config
 from pynput import keyboard
 
+KEY_MAP = {
+    'q': (0, -1),
+    'a': (0, 1),
+    'w': (1, 1),
+    's': (1, -1),
+    'e': (2, 1),
+    'd': (2, -1),
+    'r': (3, 1),
+    'f': (3, -1),
+    't': (4, 1),
+    'g': (4, -1),
+    'y': (5, 1),
+    'h': (5, -1),
+    'u': (6, 1),
+    'j': (6, -1),
+    'i': (7, 1),
+    'k': (7, -1)
+}
+
 class Teleop2:
     def __init__(self):
         rospy.init_node("teleop_2")
@@ -16,27 +35,27 @@ class Teleop2:
         self.reset_proxy = rospy.ServiceProxy(config.RESET_SERVICE, cembra.srv.ResetEnv2)
         self.action_proxy = rospy.ServiceProxy(config.ACTION_SERVICE, cembra.srv.ActionEnv2)
 
-        self.actions = [1, 1, 1, 1, 1, 1, 1, 1]
+        self.actions = [0, 0, 0, 0, 0, 0, 0, 0]
 
     def start(self):
-        # self.reset_proxy()
+        self.reset_proxy()
 
         listener = keyboard.Listener(
             on_press=self.on_press)
         listener.start()
     
         while not rospy.is_shutdown():
-            passd
+            self.action_proxy(self.actions)
+            self.actions = [0, 0, 0, 0, 0, 0, 0, 0]
 
         listener.stop()
 
     def on_press(self, key):
-        try:
-            print('alphanumeric key {0} pressed'.format(
-                key.char))
-        except AttributeError:
-            print('special key {0} pressed'.format(
-                key))
+        a = KEY_MAP[key.char]
+
+        if a is not None:
+            self.actions[a[0]] = a[1]
+
 
 
 
