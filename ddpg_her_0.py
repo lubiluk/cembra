e63c6b3015bca992fff598e044ee0988e0b78ff0
Paweł Gajewski <lubiluk@gmail.com>
2020-11-02 18:33:54 +0100

    gym based training

---
+++ ddpg_her_0.py
@@ -0,0 +1,27 @@
+from algos import ddpg_her
+import torch as th
+import gym
+import gym_hsr_gazebo
+
+env_fn = lambda : gym.make('HsrbPush-v0')
+
+ac_kwargs = dict(hidden_sizes=[64, 64], activation=th.nn.ReLU)
+
+logger_kwargs = dict(
+    output_dir='data/hsrb_push_ddpg_her_0', 
+    exp_name='hsrb_push_ddpg_her_0')
+
+ddpg_her(
+    env_fn=env_fn, 
+    ac_kwargs=ac_kwargs, 
+    steps_per_epoch=15000, 
+    epochs=200, 
+    batch_size=256,
+    replay_size=1000000,
+    start_steps=1000,
+    gamma=0.95,
+    q_lr=0.001,
+    pi_lr=0.001,
+    num_additional_goals=4,
+    goal_selection_strategy='future',
+    logger_kwargs=logger_kwargs)
