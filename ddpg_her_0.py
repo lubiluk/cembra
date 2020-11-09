c8755a5ec298fe818bfed02821398824dcdb325d
Paweł Gajewski <lubiluk@gmail.com>
2020-11-09 17:57:32 +0100

    grasp hack removed

--- ddpg_her_0.py
+++ ddpg_her_0.py
@@ -7,7 +7,7 @@ import gym_hsr_gazebo
 def env_fn(): return gym.make('HsrbPush-v0')
 
 
-ac_kwargs = dict(hidden_sizes=[64, 64], activation=th.nn.ReLU)
+ac_kwargs = dict(hidden_sizes=[512, 512, 512], activation=th.nn.ReLU)
 
 logger_kwargs = dict(
     output_dir='data/hsrb_push_ddpg_her_0',
@@ -20,12 +20,13 @@ ddpg_her(
     epochs=200,
     batch_size=256,
     replay_size=1000000,
-    start_steps=1000,
+    start_steps=0,
+    update_after=0,
     gamma=0.95,
     q_lr=0.001,
     pi_lr=0.001,
     num_additional_goals=4,
     goal_selection_strategy='future',
     logger_kwargs=logger_kwargs,
-    demos=['data/1.hdf5', 'data/2.hdf5', 'data/3.hdf5', 'data/4.hdf5',
-           'data/5.hdf5', 'data/6.hdf5', 'data/7.hdf5', 'data/8.hdf5'])
+    demos=['data/demos/1.hdf5', 'data/demos/2.hdf5', 'data/demos/3.hdf5', 'data/demos/4.hdf5',
+           'data/demos/5.hdf5', 'data/demos/6.hdf5', 'data/demos/7.hdf5', 'data/demos/8.hdf5'])
